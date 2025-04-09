b7772e6f402a71d0a055dcec80904d456c631a03
Tanishka Chaudhary <chaudharytanishka38@gmail.com>
2025-04-09 19:39:39 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import Greeting from "./Greetings";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Greeting></Greeting>
     </div>
   )
 }
---
+++ src/components/Greetings.js
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+
+
+
+const Greeting = () =>{
+
+    let [value,setValue] = useState();
+
+
+    function displayValue(e){
+        let val = e.target.value
+        setValue(val);
+    }
+
+
+    return (
+        <div>
+          Enter your name : <br></br>
+         <input type="text" onChange={displayValue}></input>
+
+         <p>
+           
+           {value ? `Hello, ${value}!` : " "}
+           
+            </p>
+        </div>
+    )
+}
+
+
+export default Greeting;
